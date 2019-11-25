ffafccdfbc68e8088dc7cdccfa576f4d66b88a9c
Linus Lundberg <linluw-3@student.ltu.se>
2019-11-25 19:58:25 +0100

    nu kör vi.

--- Task1_String_length/String_length.cpp
+++ Task1_String_length/String_length.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 
+using namespace std;
 
+int string_length(char* ptr)
+{
+	int count = 0;
+	for (int i = 0; i = *ptr != '\0'; ptr++)
+	{
+		count++;
+	}
 
-
-
-
-
-
-
+	return count;
+}
 
 
 int main()
 {
 
+	char arr[] = "computergames";
+	char* ptr = arr;
 
-
-
+	cout << string_length(ptr) << endl;
 
 }
---
+++ Task2_Average_salaries/Average_salaries.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+
+float average_salary()
+{
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+//kör
+
+int main()
+{
+
+	int numItems;
+	cout << "How many employees work in this department? ";
+	cin >> numItems; 
+	int* arr = new int[numItems];
+	cout << endl;
+
+
+
+
+
+
+
+
+	delete[] arr;
+
+}
