e8d796ee32c593d48b2373c63f0ef19e2d1f5abc
Linus Lundberg <linluw-3@student.ltu.se>
2019-11-25 23:31:18 +0100

    Task 5 done

--- Task1_String_length/String_length.cpp
+++ Task1_String_length/String_length.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int string_length(char* ptr)
 {
 	int count = 0;
+	//This looks weird but it works. You'll see an even weirder at Task 5.
 	for (int i = 0; i = (*ptr != '\0'); ptr++)
 	{
 		count++;
---
+++ Task5_Character_replacement_with_pointers/CharRepWithPointers.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+
+void replace_chars(char* str, char out, char in)
+{
+	//This solution feels like it can be 100 times better
+	for (int i = 0; i = (*str != '\0'); str++)
+	{
+		if (*str == out)
+		{
+			*str = in;
+		}
+
+
+		if (*(str + i) == out)
+		{
+			*(str + i) = in;
+
+		}
+	}
+}
+
+
+
+int main()
+{
+	char out, in;
+
+	char arr[] = "aibohphobia";
+	char* str = arr;
+
+	cout << "Char array is: " << str << endl << endl;
+
+	cout << "What character do you want to replace?: ";
+	cin >> out;
+
+	cout << "What character do you want to replace that with?: ";
+	cin >> in;
+
+	replace_chars(str, out, in);
+	cout << endl;
+
+	cout << "Char array is " << str << endl;
+	
+}
